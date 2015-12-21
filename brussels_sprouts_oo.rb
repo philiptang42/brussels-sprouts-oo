715ff1ec90fa3567c376cc2ecf901a6c7b1a16d6
philiptang42 <philiptang42@gmail.com>
2015-12-20 19:46:11 -0500

    argument specifics

--- brussels_sprouts_oo.rb
+++ brussels_sprouts_oo.rb
@@ -20,4 +20,25 @@ end
 
 ingredient = Ingredient.new(47.0, "lb(s)", "Brussels Sprouts")
 
+name = "Roasted Brussels Sprouts"
+
+ingredients = [
+    Ingredient.new(1.5, "lb(s)", "Brussels sprouts"),
+    Ingredient.new(3.0, "tbspn(s)", "Good olive oil"),
+    Ingredient.new(0.75, "tspn(s)", "Kosher salt"),
+    Ingredient.new(0.5, "tspn(s)", "Freshly ground black pepper")
+]
+
+instructions = [
+    "Preheat oven to 400 degrees F.",
+    "Cut off the brown ends of the Brussels sprouts.",
+    "Pull off any yellow outer leaves.",
+    "Mix them in a bowl with the olive oil, salt and pepper.",
+    "Pour them on a sheet pan and roast for 35 to 40 minutes.",
+    "They should be until crisp on the outside and tender on the inside.",
+    "Shake the pan from time to time to brown the sprouts evenly.",
+    "Sprinkle with more kosher salt ( I like these salty like French fries).",
+    "Serve and enjoy!"
+    ]
+
 puts ingredient.summary
